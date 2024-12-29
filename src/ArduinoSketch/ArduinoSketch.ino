1e8d9e682fa61d7abcea6c6d15f08864cfa1ec5d
bparento <sp3643@drexel.edu>
2024-12-29 16:17:25 -0500

    Testing

--- src/ArduinoSketch/ArduinoSketch.ino
+++ src/ArduinoSketch/ArduinoSketch.ino
@@ -1,11 +1,11 @@
-#include <Arduino.h>
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+TESTING
