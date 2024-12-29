e6d149adce98a408dd727049f895c2ea15526d19
bparento <sp3643@drexel.edu>
2024-12-29 16:15:49 -0500

    Included library

--- src/ArduinoSketch/ArduinoSketch.ino
+++ src/ArduinoSketch/ArduinoSketch.ino
@@ -1,6 +1,8 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
 
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
 }
 
 void loop() {
